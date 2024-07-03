f6927c3d8b6d0117fc499e0f3b6a8e43ae26cb1d
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-07-03 02:43:49 +0200

    refactor: update thunk

--- src/features/roomList/roomListUpdateOneThunk.js
+++ src/features/roomList/roomListUpdateOneThunk.js
@@ -1,15 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-const updateRoom = (room) => {
+const updateRoom = (updateRoom, list) => {
   return new Promise((resolve, rejected) => {
     setTimeout(() => {
-      resolve(room);
+      resolve(
+        list.map((room) => {
+          if (room.id === updateRoom.id)
+            return { ...updateRoom }
+      
+          return room;
+        })
+      );
     }, 200);
   })
 }
 
-export const roomListUpdateOneThunk = createAsyncThunk("roomListUpdateOne", async ({room}) => {
-  const savedRoom = await updateRoom(room);
+export const roomListUpdateOneThunk = createAsyncThunk("roomListUpdateOne", async ({room, list}) => {
+  const roomList = await updateRoom(room, list);
 
-  return savedRoom;
+  return roomList;
 })
