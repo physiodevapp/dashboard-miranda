47920b0299e9226002dad6fc6d98cc7f8b25381d
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-07-17 05:48:18 +0200

    feature: DashboardPage to Typescript

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+import { KPIsListComponent } from '../components/KPIsList/KPIsListComponent';
+import { RecentContactListComponent } from '../components/RecentContacts/RecentContactListComponent';
+import { PageElementContainerStyled } from '../components/PageElementContainerStyled';
+
+export const DashboardPage = () => {  
+  return (
+    <>
+      <PageElementContainerStyled>
+        <KPIsListComponent/>
+        <RecentContactListComponent/>
+      </PageElementContainerStyled>
+    </>
+  )
+}
